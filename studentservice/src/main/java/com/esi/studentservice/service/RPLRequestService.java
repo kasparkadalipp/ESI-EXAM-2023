3fe9abb2f6fccafcf12ad9719fb18ac7712d9d34
kasparkadalipp <kaspar.kadalipp@gmail.com>
2023-05-22 16:14:14 +0300

    Task 3.4

--- studentservice/src/main/java/com/esi/studentservice/service/RPLRequestService.java
+++ studentservice/src/main/java/com/esi/studentservice/service/RPLRequestService.java
@@ -7,6 +7,7 @@ import com.esi.studentservice.repository.RPLRequestRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RPLRequestService {
 
+    private final KafkaTemplate<String, RPLRequestDto> kafkaTemplate;
     @Autowired
     private RPLRequestRepository RPLRequestRepository;
 
@@ -29,6 +31,9 @@ public class RPLRequestService {
         rPLRequestDto.setRPLRequestStatus(RPLRequestStatus.Submitted);
         RPLRequest rplRequest = mapRequestsDtoToRPL(rPLRequestDto);
         RPLRequestRepository.save(rplRequest);
+        log.info("Request saved to database");
+        kafkaTemplate.send("StdRequestSubmitted", rPLRequestDto);
+        log.info("Send StdRequestSubmitted: {}", rPLRequestDto);
     }
 
     private RPLRequestDto mapToRPLRequestsDto(RPLRequest rPLRequest) {
